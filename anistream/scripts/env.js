6574989d3ab3b6c47f9a00a5c45d5ca83b2f211c
Asta <70468074+F4sta@users.noreply.github.com>
2025-03-05 10:01:49 +0100

    Update env.js

--- anistream/scripts/env.js
+++ anistream/scripts/env.js
@@ -2,4 +2,4 @@
 const local_api_url = 'http://localhost:4000'
 const online_api_url = 'https://hianime-api-pi.vercel.app'
 const proxy_url = `https://crossorigin.me/${online_api_url}`
-var api_url = proxy_url
+var api_url = online_api_url
